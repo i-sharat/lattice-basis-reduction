8e158262f028352126186b3698710eb6c02be739
sharat1105 <Sharat@v1022-rn-208-91.campus-dynamic.uwaterloo.ca>
2015-12-23 05:32:01 -0500

    constants for BKZ algorithm

---
+++ BKZ_constants.py
@@ -0,0 +1,16 @@
+eps = 1e-5
+
+# for LLL
+delta = 0.99
+
+# for generating test cases
+q = 1e2
+
+# for checking whether a set of vectors are linearly indepedent
+epsilonForIndependence = 1e-6
+
+# for checking if two bases are equivalent
+epsilonForBasisComparison = 1e-4
+
+BKZBlockSize = 20
+
